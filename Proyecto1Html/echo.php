abc65fbcb386b31dfff1f4e1e4a5ba5aede23554
aitzolsoto <56816883+aitzolsoto@users.noreply.github.com>
2020-09-24 13:59:04 +0200

    Add files via upload

---
+++ Proyecto1Html/echo.php
@@ -0,0 +1,12 @@
+<?php
+
+/*echo "<h1>";*/
+
+if (isset($_GET["correo"]))
+    echo $_GET["correo"];
+
+    /*echo "</h1>";*/
+
+echo "<br>";
+if (isset($_GET["password"]))
+    echo $_GET["password"];
